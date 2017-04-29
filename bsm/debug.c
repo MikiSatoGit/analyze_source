15247305d4edaea46f424e1043e8b62068f8324c
MikiSatoGit <miki.sam.sato@gmail.com>
2017-04-28 20:34:42 -0400

    debugging
    updated debug file only

--- bsm/debug.c
+++ bsm/debug.c
@@ -1,38 +1,16 @@
-VD fn_btt_atd_trailerobj_tracking(
-	NORMAL_BSM pst_a_trailer_obj[TRAILER_BUFFSIZE],
-	S4 as4_a_trailer_obj_num
-	,FL fl_a_doa_pow_ave[2][3]
-	,FL fl_a_doa_pow_dev[2][3]
-	,S4 s4_a_doa_bf_peak_bin
-	,FL fl_a_doa_bf_peak_pow
+VD fn_miki_test3(
+	type1 arg1
+	,type2 arg2
 )
 {
-
-	/* If there is object be detected more than half of history checking cycle */
-	if( as4_a_trk_num() > (S4)BTT_OBJ_DETECT_CYC1 ){
-		if( fl_abs(afl_a_trailer_obj_rx_ave) < BTT_OBJ_X_RANGE
-		&&  afl_a_trailer_obj_ry_ave < BTT_OBJ_Y_RANGE_FORWARD
-		&&  afl_a_trailer_obj_ry_ave > BTT_OBJ_Y_RANGE_BACKWARD
-		&&  fl_abs(afl_a_trailer_obj_vy_ave) < BTT_OBJ_VY 
-		&&  afl_a_trailer_obj_rx_dev < BTT_OBJ_DEV_TH 
-		&&  afl_a_trailer_obj_vy_dev < BTT_OBJ_DEV_TH ){
-			MIKI_TEST = 1;
-		}
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	miki2_1 = subsub_1(
+		(S1)arg2_1_1,
+		arg2_1_2 );
+	miki2_2 
+	= subsub_2(
+		(S1)arg2_2_1,
+		arg2_2_2
+	);
 	return;
 }
 
